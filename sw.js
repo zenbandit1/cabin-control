c2846e73ced10e4f3db68b2b3fc255b692557e44
zenbandit1 <109433184+zenbandit1@users.noreply.github.com>
2025-11-07 10:22:56 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,35 @@
+// Service Worker for Cabin Control PWA (GitHub Pages-ready)
+const CACHE_NAME = 'cabin-control-v2';
+const urlsToCache = [
+  './cabin_control_pwa.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
